83e41375f92b645dec93914d3b0f231cb93b690d
Zan Smirnov <c15437072@mydit.ie>
2016-11-17 12:11:23 +0000

    Added filereader.

---
+++ FileReader.java
@@ -0,0 +1,11 @@
+/*
+    Reads in a file and puts all of the bytes in an array.
+ */
+
+public class FileReader {
+    String path;
+
+    FileReader(String path) {
+        this.path = path;
+    }
+}
