97a9303189a6b223505002e914e76389d1a2e30f
Zan Smirnov <c15437072@mydit.ie>
2016-11-17 13:00:15 +0000

    Added test program.

--- Test.java
+++ Test.java
@@ -3,20 +3,13 @@ import processing.core.*;
 
 
 public class Test extends PApplet{
-
-    public static byte [] CODE = { 0x55, 0x48, (byte) 0x8b, 0x05, (byte) 0xb8,
-            0x13, 0x00, 0x00 };
-
     public void settings(){
         size(1000, 1000);
     }
 
     public void setup() {
-        Capstone cs = new Capstone(Capstone.CS_ARCH_X86, Capstone.CS_MODE_64);
-        Capstone.CsInsn[] allInsn = cs.disasm(CODE, 0x1000);
-        for (int i=0; i<allInsn.length; i++)
-            System.out.printf("0x%x:\t%s\t%s\n", allInsn[i].address,
-                    allInsn[i].mnemonic, allInsn[i].opStr);
+        byte[] bytes = FileReader.readFile("C:\\Users\\Ecoste\\IdeaProjects\\i-didn-t-think-of-a-name-yet\\testHello32.exe");
+        System.out.println(bytes.length);
     }
 
     public void draw(){
@@ -28,3 +21,29 @@ public class Test extends PApplet{
         PApplet.main("Test");
     }
 }
+
+
+
+//    public static byte [] CODE = { 0x55, 0x48, (byte) 0x8b, 0x05, (byte) 0xb8,
+//            0x13, 0x00, 0x00 };
+//
+//    public void settings(){
+//        size(1000, 1000);
+//    }
+//
+//    public void setup() {
+//        Capstone cs = new Capstone(Capstone.CS_ARCH_X86, Capstone.CS_MODE_64);
+//        Capstone.CsInsn[] allInsn = cs.disasm(CODE, 0x1000);
+//        for (int i=0; i<allInsn.length; i++)
+//            System.out.printf("0x%x:\t%s\t%s\n", allInsn[i].address,
+//                    allInsn[i].mnemonic, allInsn[i].opStr);
+//    }
+//
+//    public void draw(){
+//        background(0);
+//        ellipse(mouseX, mouseY, 20, 20);
+//    }
+//
+//    public static void main(String... args){
+//        PApplet.main("Test");
+//    }
