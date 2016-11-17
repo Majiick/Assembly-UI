dd1af66469df7884dfeffaf03de0f8df416c858e
Zan Smirnov <c15437072@mydit.ie>
2016-11-17 14:01:42 +0000

    Testing on hello world successful.

--- Test.java
+++ Test.java
@@ -1,15 +1,27 @@
 import capstone.Capstone;
 import processing.core.*;
+import java.util.Arrays;
+import static javax.xml.bind.DatatypeConverter.printHexBinary;
 
 
 public class Test extends PApplet{
+    final static int entryPointOfProgram = 0x12A0 - 0x1000;
+
     public void settings(){
         size(1000, 1000);
     }
 
     public void setup() {
-        byte[] bytes = FileReader.readFile("C:\\Users\\Ecoste\\IdeaProjects\\i-didn-t-think-of-a-name-yet\\testHello32.exe");
-        System.out.println(bytes.length);
+        byte[] bytes = FileReader.readFile("C:\\Users\\Ecoste\\IdeaProjects\\i-didn-t-think-of-a-name-yet\\helloWorld32.bin");
+        System.out.println("File size: " + bytes.length);
+        System.out.println("Byte at entry point: " + String.format("%02x", bytes[entryPointOfProgram]));
+        printBytes(Arrays.copyOfRange(bytes, entryPointOfProgram, entryPointOfProgram + 0x15));
+
+        Capstone cs = new Capstone(Capstone.CS_ARCH_X86, Capstone.CS_MODE_32);
+        Capstone.CsInsn[] allInsn = cs.disasm(Arrays.copyOfRange(bytes, entryPointOfProgram, entryPointOfProgram + 0x15), entryPointOfProgram, 0x15);
+        for (int i=0; i<allInsn.length; i++)
+            System.out.printf("0x%x:\t%s\t%s\n", allInsn[i].address,
+                    allInsn[i].mnemonic, allInsn[i].opStr);
     }
 
     public void draw(){
@@ -20,6 +32,14 @@ public class Test extends PApplet{
     public static void main(String... args){
         PApplet.main("Test");
     }
+
+    public static void printBytes(byte[] bytes) {
+        for (byte b : bytes) {
+            System.out.print(String.format("%02x", b));
+        }
+        System.out.print("\n");
+    }
+
 }
 
 
